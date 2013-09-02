e989d50e482de239eb5f5a5bcc94e978aafd5dbc
mike suarez <mikemps@gmail.com>
2013-09-02 12:35:15 -0400

    cdn

--- src/doble9.com/public/js/requirejs_config.js
+++ src/doble9.com/public/js/requirejs_config.js
@@ -1,13 +1,13 @@
 window.requirejs_config = {
     baseUrl: '/coffee/',
     paths: {
-        jquery: '/js/jquery',
-        cs: '/js/cs',
-        text: '/js/text',
-        'coffee-script': '/js/coffee-script',
-        underscore: '/js/underscore',
-        bootstrap: '/js/bootstrap.min',
-        backbone: '/js/backbone'
+        jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
+        cs: '//cdnjs.cloudflare.com/ajax/libs/require-cs/0.4.2/cs',
+        text: '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.10/text',
+        'coffee-script': '//cdnjs.cloudflare.com/ajax/libs/coffee-script/1.6.3/coffee-script.min',
+        underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.1/underscore-min',
+        bootstrap: '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min',
+        backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min'
     },
     shim: {
         underscore: {
