c8e59910270a5dd002e32c1bcf61cd55841922b4
maykels <maykel_suarez@ultimatesoftware.com>
2019-08-17 18:13:24 -0400

    moar cleanup

--- v3/doble9_engine/lib/doble9_engine/player.ex
+++ v3/doble9_engine/lib/doble9_engine/player.ex
@@ -9,7 +9,6 @@ defmodule Doble9Engine.Player do
   def start_bot bot, game do GenServer.start_link __MODULE__, %{name: bot, bot: true, game: game}, name: bot end
   def new_game player, game do GenServer.call player, {:new_game, game} end
 
-  def join player, game do GenServer.call player, {:join, game} end
   def pick player do GenServer.call player, :pick end
   def turn player, heads \\ [] do GenServer.cast player, {:turn, heads} end
   def play player, domino do GenServer.call player, {:play, domino}  end
@@ -19,6 +18,7 @@ defmodule Doble9Engine.Player do
     dominoes: [],
     turn: nil,
     played: nil,
+    knocked: nil
   }
 
   def init %{bot: true} = player do
@@ -29,6 +29,14 @@ defmodule Doble9Engine.Player do
 
   def new player do {:ok, merge(@defaults, player)} end
 
+  def handle_call {:new_game, game}, _, %{name: name} = player do
+    created Game.create(game, name), game, player
+  end
+
+  def handle_call {:play, domino}, _, %{name: name, game: game} = player do
+    {:reply, :ok, played(Game.play(game, name, domino), player, domino)}
+  end
+
   def handle_cast {:turn, heads}, %{bot: true} = player do
     send self, :play
     {:noreply, turned(heads, player)}
@@ -37,63 +45,24 @@ defmodule Doble9Engine.Player do
     {:noreply, turned(heads, player)}
   end
 
-  def handle_call {:new_game, game}, _, %{name: name} = player do
-    created Game.create(game, name), game, player
-  end
-
-  def handle_call({:join, _}, _, %{game: game} = player) when game != nil do
-    { :reply, { :error, "already in game" }, player }
-  end
-  def handle_call {:join, game}, _, %{name: name} = player do
-    joined Game.join(game, name), player, game
-  end
-
-  def handle_call :pick, _, %{name: name, game: game} = player do
-    reply picked Game.pick(game, name), player
-  end
-
-  def handle_call {:play, domino}, _, %{name: name, dominoes:  dominoes, game: game} = player do
-    cond do
-      game == nil -> { :reply, { :error, "need to join a game first" }, player }
-      domino not in dominoes -> { :reply, {:error, "forro!!"}, player}
-      :ok -> {:reply, :ok, played(Game.play(game, name, domino), player, domino)}
-    end
-  end
-
-  def reply {:ok, player} do {:reply, :ok, player} end
-  def reply {{:ok, response}, player} do {:reply, {:ok, response}, player} end
-  def reply {error, player} do {:reply, error, player} end
-
   def handle_info :pick, %{name: name, game: game} = player do
-    {:ok, player} = picked Game.pick(game), player
-    {:noreply, player}
+    {:noreply, picked(Game.pick(game), player)}
   end
 
-  def handle_info {:play, heads}, player do
+  def handle_info :play, player do
     {:noreply, player}
   end
 
   def created {:ok, _}, game, player do
     send self, :pick
-    { :reply, :ok, %{player | game: game}}
-  end
-
-  def joined :ok, player, game do
-    { :reply, :ok, %{player | game: game}}
-  end
-
-  def joined error, player, _ do
-    { :reply, error, player }
+    {:reply, :ok, %{player | game: game}}
   end
 
   def picked({:ok, dominoes}, %{turn: turn} = player) when turn != nil do
-    { :ok, %{ player | dominoes: dominoes, turn: %{turn | choices: dominoes}}}
+    %{player | dominoes: dominoes, turn: %{turn | choices: dominoes}}
   end
   def picked {:ok, dominoes}, player do
-    { :ok, %{ player | dominoes: dominoes}}
-  end
-  def picked error, player do
-    { error, player }
+    %{player | dominoes: dominoes}
   end
 
   def played :ok, player, domino do
