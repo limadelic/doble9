81968a0e76ddd6b1bfac0c28fee766e7ddcdb38e
maykels <maykel_suarez@ultimatesoftware.com>
2019-08-24 17:21:59 -0400

    allowing to play

--- v3/doble9_engine/lib/doble9_engine/ui/ui.exs
+++ v3/doble9_engine/lib/doble9_engine/ui/ui.exs
@@ -4,17 +4,31 @@ defmodule Doble9Engine.UI do
   alias Doble9Engine.UI.Player
 
   import Ratatouille.View
-  import Doble9Engine.Player, only: [login: 1, new_game: 2]
+  import Enum, only: [map: 2]
+  import Doble9Engine.Player, only: [login: 1, new_game: 2, play: 2]
   import Doble9Engine.Helpers
 
   @game :calle8
   @player :mike
+  @numbers 48..67
 
   def init %{window: window} do
     login @player
     new_game @player, @game
-    %{window: window, game: the(@game)}
+    %{window: window, game: the(@game), playing: nil}
   end
+
+  def update(%{playing: nil} = game, {:event, %{ch: ch}}) when ch in @numbers do
+    %{game | playing: ch - 48}
+  end
+
+  def update(%{playing: head} = game, {:event, %{ch: ch}}) when ch in @numbers do
+    play @player, [head, ch - 48]
+    :timer.sleep 10
+    %{game | game: the(@game), playing: nil}
+  end
+
+  def update game, {_, x} do i x; game end
 
   def render %{game: %{players: [player, right, top, left]}, window: %{height: height, width: width} = window} do
     view do
