c0c5184829496c603deddf0147894047a7bbbd2f
maykels <maykel_suarez@ultimatesoftware.com>
2019-08-24 16:07:19 -0400

    drawing all players

--- v3/doble9_engine/lib/doble9_engine/ui.exs
+++ v3/doble9_engine/lib/doble9_engine/ui.exs
@@ -14,16 +14,46 @@ defmodule Doble9Engine.UI do
   def init %{window: window} do
     Player.login @player
     Player.new_game @player, @game
-    %{window: window, player: the(@player)}
+    %{window: window, game: the(@game)}
   end
 
-  def render %{window: %{height: height, width: width}, player: player} do
+  def render %{game: %{players: [player, right, top, left]}, window: %{height: height, width: width}} do
     view do
       canvas height: height, width: width do
         [
-          render(%{player: player, height: height, width: width})
+          render(%{player: the(top), at: :top, height: height, width: width}),
+          render(%{player: the(left), at: :left, height: height, width: width}),
+          render(%{player: the(right), at: :right, height: height, width: width}),
+          render(%{player: the(player), height: height, width: width}),
         ]
       end
+    end
+  end
+
+  def render %{player: %{dominoes: dominoes}, at: :top, width: window_width} do
+    {width, height} = measure frame @size, :y
+    left = div(window_width - (width * length(dominoes)), 2)
+
+    for {domino, i} <- with_index dominoes do
+      render %{domino: :back, size: @size, axis: :y, left: left + i * width, top: 0}
+    end
+  end
+
+  def render %{player: %{dominoes: dominoes}, at: :left, height: window_height} do
+    {width, height} = measure frame @size, :x
+    top = div(window_height - (height * length(dominoes)), 2)
+
+    for {domino, i} <- with_index dominoes do
+      render %{domino: :back, size: @size, axis: :x, left: 0, top: top + i * height}
+    end
+  end
+
+  def render %{player: %{dominoes: dominoes}, at: :right, width: window_width, height: window_height} do
+    {width, height} = measure frame @size, :x
+    top = div(window_height - (height * length(dominoes)), 2)
+
+    for {domino, i} <- with_index dominoes do
+      render %{domino: :back, size: @size, axis: :x, left: window_width - width, top: top + i * height}
     end
   end
 
@@ -42,6 +72,12 @@ defmodule Doble9Engine.UI do
       render(%{frame: frame(size, axis), left: left, top: top}),
       render(%{head: number(h, size, axis), left: left, top: top, }),
       render(%{tail: number(t, size, axis), axis: axis, left: left, top: top, }),
+    ]
+  end
+
+  def render %{domino: :back, size: size, axis: axis, left: left, top: top} = domino do
+    [
+      render(%{frame: back(size, axis), left: left, top: top}),
     ]
   end
 
