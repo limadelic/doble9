a6a37567a3f96e43ef3c30a0582d786b0ffd8ef9
maykels <maykel_suarez@ultimatesoftware.com>
2019-08-26 20:06:37 -0400

    refactored opponents render

--- v3/doble9_engine/lib/doble9_engine/ui/player.ex
+++ v3/doble9_engine/lib/doble9_engine/ui/player.ex
@@ -1,32 +1,35 @@
 defmodule Doble9Engine.UI.Player do
 
   alias Doble9Engine.UI.Domino
+  import Map, only: [merge: 2]
   import Enum, only: [with_index: 1]
   import Doble9Engine.UI.Assets
   import Doble9Engine.UI.Arranger
 
-  def render %{player: %{dominoes: dominoes}, at: :top, window: window, show: show} do
-    left = div(window.width - (width(:y) * length(dominoes)), 2)
-
+  def render %{player: %{dominoes: dominoes}, axis: :x, window: %{width: width}, show: show, top: top} do
+    left = div(width - (width(:y) * length(dominoes)), 2)
     for {domino, i} <- with_index dominoes do
-      Domino.render %{domino: domino, show: show, axis: :y, left: left + i * width(:y), top: 0}
+      Domino.render %{domino: domino, show: show, axis: :y, left: left + i * width(:y), top: top}
     end
   end
 
-  def render %{player: %{dominoes: dominoes}, at: :left, window: window, show: show} do
-    top = div(window.height - (height(:x) * length(dominoes)), 2)
-
+  def render %{player: %{dominoes: dominoes}, axis: :y, window: %{height: height}, show: show, left: left} do
+    top = div(height - (height(:x) * length(dominoes)), 2)
     for {domino, i} <- with_index dominoes do
-      Domino.render %{domino: domino, show: show, axis: :x, left: 0, top: top + i * height(:x)}
+      Domino.render %{domino: domino, show: show, axis: :x, left: left, top: top + i * height(:x)}
     end
   end
 
-  def render %{player: %{dominoes: dominoes}, at: :right, window: window, show: show} do
-    top = div(window.height - (height(:x) * length(dominoes)), 2)
+  def render %{at: :top} = player do
+    render merge player, %{axis: :x, top: 0}
+  end
 
-    for {domino, i} <- with_index dominoes do
-      Domino.render %{domino: domino, show: show, axis: :x, left: window.width - width(:x), top: top + i * height(:x)}
-    end
+  def render %{at: :left} = player do
+    render merge player, %{axis: :y, left: 0}
+  end
+
+  def render %{at: :right, window: %{width: width}} = player do
+    render merge player, %{axis: :y, left: width - width(:x)}
   end
 
   def render %{player: %{dominoes: dominoes, turn: %{choices: [first|_] = choices}}, at: :bottom, window: window} do
