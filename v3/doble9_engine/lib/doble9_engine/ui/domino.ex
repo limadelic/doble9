bf964b679742e1cee3db18bf2eea53a6140d8ce0
maykels <maykel_suarez@ultimatesoftware.com>
2019-08-26 23:39:37 -0400

    refactored domino

--- v3/doble9_engine/lib/doble9_engine/ui/domino.ex
+++ v3/doble9_engine/lib/doble9_engine/ui/domino.ex
@@ -7,46 +7,8 @@ defmodule Doble9Engine.UI.Domino do
 
   def from x do %{domino: x.domino, axis: x.axis, left: x.left, top: x.top, show: x.show} end
 
-  def render %{show: false, axis: axis, left: left, top: top} = domino do
-    [
-      render(%{frame: back(axis), left: left, top: top}),
-    ]
-  end
-
-  def render %{domino: [h,t], axis: axis, left: left, top: top, selected: true} = domino do
-    [
-      render(%{frame: selected(axis), left: left, top: top}),
-      render(%{head: number(h, axis), left: left, top: top, }),
-      render(%{tail: number(t, axis), axis: axis, left: left, top: top, }),
-    ]
-  end
-
-  def render %{domino: [h,t], axis: axis, left: left, top: top} = domino do
-    [
-      render(%{frame: frame(axis), left: left, top: top}),
-      render(%{head: number(h, axis), left: left, top: top, }),
-      render(%{tail: number(t, axis), axis: axis, left: left, top: top, }),
-    ]
-  end
-
-  def render %{frame: frame, left: left, top: top} do
-    render %{glyph: frame, width: width(frame), height: height(frame), left: left, top: top, bk: false}
-  end
-
-  def render %{head: head, left: left, top: top} do
-    render %{glyph: head, width: width(head), height: height(head), left: left + 1, top: top + 1, bk: true}
-  end
-
-  def render %{tail: tail, left: left, top: top, axis: :x} do
-    render %{glyph: tail, width: width(tail), height: height(tail), left: left + width(tail) + 2, top: top + 1, bk: true}
-  end
-
-  def render %{tail: tail, left: left, top: top, axis: :y} do
-    render %{glyph: tail, width: width(tail), height: height(tail), left: left + 1, top: top + height(tail) + 2, bk: true}
-  end
-
-  def render %{glyph: glyph, width: width, height: height, left: left, top: top, bk: bk} do
-    for x <- 0..width - 1, y <- 0..height - 1 do
+  def render %{glyph: glyph, left: left, top: top, bk: bk} do
+    for x <- 0..width(glyph) - 1, y <- 0..height(glyph) - 1 do
       render %{char: char_at(glyph, x, y), x: left + x, y: top + y, bk: bk}
     end
   end
@@ -55,6 +17,44 @@ defmodule Doble9Engine.UI.Domino do
 
   def render %{char: char, x: x, y: y} do
     canvas_cell x: x, y: y, char: char
+  end
+
+  def render %{frame: frame} = domino do
+    render merge domino, %{glyph: frame, width: width(frame), height: height(frame), bk: false}
+  end
+
+  def render %{head: head, left: left, top: top} = domino do
+    render merge domino, %{glyph: head, left: left + 1, top: top + 1, bk: true}
+  end
+
+  def render %{tail: tail, left: left, top: top, axis: :x} = domino do
+    render merge domino, %{glyph: tail, left: left + width(tail) + 2, top: top + 1, bk: true}
+  end
+
+  def render %{tail: tail, left: left, top: top, axis: :y} = domino do
+    render merge domino, %{glyph: tail, left: left + 1, top: top + height(tail) + 2, bk: true}
+  end
+
+  def render %{show: false, axis: axis} = domino do
+    [
+      render(merge domino, %{frame: back(axis)}),
+    ]
+  end
+
+  def render %{selected: true, domino: [h,t], axis: axis} = domino do
+    [
+      render(merge domino, %{frame: selected(axis)}),
+      render(merge domino, %{head: number(h, axis)}),
+      render(merge domino, %{tail: number(t, axis)}),
+    ]
+  end
+
+  def render %{domino: [h,t], axis: axis} = domino do
+    [
+      render(merge domino, %{frame: frame(axis)}),
+      render(merge domino, %{head: number(h, axis)}),
+      render(merge domino, %{tail: number(t, axis)}),
+    ]
   end
 
 end
