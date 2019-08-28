04225669046ccac5e9169a0c61dc0569a0113b90
maykels <maykel_suarez@ultimatesoftware.com>
2019-08-27 22:04:38 -0400

    refactored controller

--- v3/doble9_engine/lib/doble9_engine/ui/controller.ex
+++ v3/doble9_engine/lib/doble9_engine/ui/controller.ex
@@ -2,7 +2,7 @@ defmodule Doble9Engine.UI.Controller do
 
   import Ratatouille.Constants, only: [key: 1]
   import Enum, only: [map: 2]
-  import Doble9Engine.Player, only: [login: 1, new_game: 2, play: 2]
+  import Doble9Engine.Player, only: [login: 1, new_game: 2, play: 2, knock: 1]
   import Doble9Engine.Helpers
 
   @game :calle8
@@ -13,12 +13,20 @@ defmodule Doble9Engine.UI.Controller do
   @down key(:arrow_down)
   @left key(:arrow_left)
   @right key(:arrow_right)
-  @arrows [@up, @down, @left, @right]
+  @space key(:space)
+  @enter key(:enter)
+  @play [@space, @up]
+  @knock [@space, @enter]
 
   def init %{window: window} do
     login @player
     new_game @player, @game
-    %{window: window, game: the(@game), player: the(@player), playing: nil, selected: selected(the(@player))}
+    update %{window: window, game: nil, player: nil, playing: nil, selected: nil}
+  end
+
+  def update game do
+    :timer.sleep 10
+    %{game | game: the(@game), player: the(@player), playing: nil, selected: selected(the(@player))}
   end
 
   def update %{selected: selected, player: %{turn: %{choices: choices}}} = game, {_, %{key: @right}} do
@@ -35,14 +43,17 @@ defmodule Doble9Engine.UI.Controller do
 
   def update(%{playing: head} = game, {:event, %{ch: ch}}) when ch in @numbers do
     play @player, [head, ch - 48]
-    :timer.sleep 10
-    %{game | game: the(@game), player: the(@player), playing: nil, selected: selected(the(@player))}
+    update game
   end
 
-  def update(%{playing: head, selected: domino} = game, {:event, %{key: 32}}) do
+  def update(%{selected: nil} = game, {:event, %{key: key}}) when key in @knock do
+    knock @player
+    update game
+  end
+
+  def update(%{selected: domino} = game, {:event, %{key: key}}) when domino != nil and key in @play do
     play @player, domino
-    :timer.sleep 10
-    %{game | game: the(@game), player: the(@player), playing: nil, selected: selected(the(@player))}
+    update game
   end
 
   def update game, {_, x} do i x; game end
