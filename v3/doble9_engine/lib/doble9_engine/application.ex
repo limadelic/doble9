96616fabceedabe4f35ceed0cb584f5f4bf245d9
maykels <maykel_suarez@ultimatesoftware.com>
2019-09-10 17:59:43 -0400

    defining the app

--- v3/doble9_engine/lib/doble9_engine/application.ex
+++ v3/doble9_engine/lib/doble9_engine/application.ex
@@ -7,7 +7,13 @@ defmodule Doble9Engine.Application do
 
   def start(_type, _args) do
     # List all child processes to be supervised
+    rat_opts = [
+      app: Doble9Engine.UI,
+      shutdown: {:application, :doble9},
+    ]
+
     children = [
+#      {Ratatouille.Runtime.Supervisor, runtime: rat_opts}
       # Starts a worker by calling: Doble9Engine.Worker.start_link(arg)
       # {Doble9Engine.Worker, arg}
     ]
@@ -17,4 +23,9 @@ defmodule Doble9Engine.Application do
     opts = [strategy: :one_for_one, name: Doble9Engine.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  def stop _ do
+    System.halt()
+  end
+
 end
