8c9bab9145732ecc4f58e167a28c6f206258577b
maykels <maykel_suarez@ultimatesoftware.com>
2019-09-12 19:27:09 -0400

    grouping n' arranging

--- v3/doble9_engine/lib/doble9_engine/rack.ex
+++ v3/doble9_engine/lib/doble9_engine/rack.ex
@@ -1,20 +1,30 @@
 defmodule Doble9Engine.Rack do
 
   def sort dominoes do
-    dominoes |> count |> group |> join
+    dominoes
+    |> sort_numbers_by_count
+    |> group_and_arrange_by_number
+    |> join
   end
 
-  def count dominoes do
-    9..0
-    |> Enum.map(&({&1, Enum.count(dominoes, fn x -> &1 in x end)}))
-    |> Enum.sort(fn {_,x}, {_,y} -> x >= y end)
-    |> Enum.map(fn {x,_} -> x end)
+  def sort_numbers_by_count dominoes do
+    {
+      dominoes,
+      9..0
+      |> Enum.map(&({&1, Enum.count(dominoes, fn x -> &1 in x end)}))
+      |> Enum.sort(fn {_,x}, {_,y} -> x >= y end)
+      |> Enum.map(fn {x,_} -> x end)
+    }
   end
 
-  def group [], _ do [] end
-  def group {dominoes, [number|numbers]} do
+  def group_and_arrange_by_number {[], _} do [] end
+  def group_and_arrange_by_number {dominoes, [number|numbers]} do
     matches = Enum.filter dominoes, &(number in &1)
-    matches ++ group(dominoes -- matches, numbers)
+    [arrange(matches, number)|group({dominoes -- matches, numbers})]
+  end
+
+  def arrange dominoes, number do
+    Enum.map dominoes, fn [h,t] -> number == h && [h,t] || [t,h] end
   end
 
   def join dominoes do
--- v3/doble9_engine/test/rack_test.exs
+++ v3/doble9_engine/test/rack_test.exs
@@ -5,8 +5,17 @@ defmodule RackTest do
 
   describe "Rack of dominoes" do
 
-    test "count" do
-      assert count([[9,9],[7,8],[8,8],[7,6],[8,6],[6,9]]) == [8,6,9,7,5,4,3,2,1,0]
+    @dominoes [[9,9],[7,8],[8,8],[7,6],[8,6],[6,9]]
+    @numbers_sorted_by_count [8,6,9,7,5,4,3,2,1,0]
+    @groups [[[8,7],[8,8],[8,6]],[[6,7],[6,9]],[[9,9]]]
+    @joine [[8,7],[8,8],[8,6],[7,6],[9,6]],[[9,9]]
+
+    test "sort numbers by count" do
+      assert sort_numbers_by_count(@dominoes) == {@dominoes, @numbers_sorted_by_count}
+    end
+
+    test "group and arrange by number" do
+      assert group({@dominoes, @numbers_sorted_by_count}) == @groups
     end
 
   end
