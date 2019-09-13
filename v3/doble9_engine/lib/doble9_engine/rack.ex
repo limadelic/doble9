219abe9f2668a0873be5d64290f1fc469d86931e
maykels <maykel_suarez@ultimatesoftware.com>
2019-09-12 22:05:21 -0400

    getting the group visitor!? pattern

--- v3/doble9_engine/lib/doble9_engine/rack.ex
+++ v3/doble9_engine/lib/doble9_engine/rack.ex
@@ -1,10 +1,12 @@
 defmodule Doble9Engine.Rack do
 
+  import Doble9Engine.Helpers
+
   def sort dominoes do
     dominoes
     |> sort_numbers_by_count
     |> group_and_arrange_by_number
-    |> join
+    |> join_groups
   end
 
   def sort_numbers_by_count dominoes do
@@ -20,14 +22,25 @@ defmodule Doble9Engine.Rack do
   def group_and_arrange_by_number {[], _} do [] end
   def group_and_arrange_by_number {dominoes, [number|numbers]} do
     matches = Enum.filter dominoes, &(number in &1)
-    [arrange(matches, number)|group({dominoes -- matches, numbers})]
+    [arrange(matches, number)|group_and_arrange_by_number({dominoes -- matches, numbers})]
   end
 
   def arrange dominoes, number do
     Enum.map dominoes, fn [h,t] -> number == h && [h,t] || [t,h] end
   end
 
-  def join dominoes do
+  def join_groups [group|groups] do join_groups [group], i groups end
+  def join_groups joined, [] do flatten joined end
+  def join_groups joined, [group|groups] do
+    i {:many, groups}
+    join_groups join_group(joined, group), groups
   end
+  def join_group joined, group do
+    i :single
+    joined ++ [group]
+  end
+
+  def flatten [] do [] end
+  def flatten [group|groups] do group ++ flatten(groups) end
 
 end
--- v3/doble9_engine/test/rack_test.exs
+++ v3/doble9_engine/test/rack_test.exs
@@ -7,15 +7,24 @@ defmodule RackTest do
 
     @dominoes [[9,9],[7,8],[8,8],[7,6],[8,6],[6,9]]
     @numbers_sorted_by_count [8,6,9,7,5,4,3,2,1,0]
-    @groups [[[8,7],[8,8],[8,6]],[[6,7],[6,9]],[[9,9]]]
-    @joine [[8,7],[8,8],[8,6],[7,6],[9,6]],[[9,9]]
+    @grouped_and_arranged_by_number [[[8,7],[8,8],[8,6]],[[6,7],[6,9]],[[9,9]]]
+    @flatten [[8,7],[8,8],[8,6],[6,7],[6,9],[9,9]]
+    @joined [[8,7],[8,8],[8,6],[7,6],[9,6],[9,9]]
 
     test "sort numbers by count" do
       assert sort_numbers_by_count(@dominoes) == {@dominoes, @numbers_sorted_by_count}
     end
 
     test "group and arrange by number" do
-      assert group({@dominoes, @numbers_sorted_by_count}) == @groups
+      assert group_and_arrange_by_number({@dominoes, @numbers_sorted_by_count}) == @grouped_and_arranged_by_number
+    end
+
+    test "join groups" do
+      assert join_groups(@grouped_and_arranged_by_number) == @joined
+    end
+
+    test "flatten groups" do
+      assert flatten(@grouped_and_arranged_by_number) == @flatten
     end
 
   end
