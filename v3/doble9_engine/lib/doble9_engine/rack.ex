27c16e1562c3d79f1e4cec4c9be508037419bc6b
maykels <maykel_suarez@ultimatesoftware.com>
2019-09-18 18:16:57 -0400

    quit on rack for now :|

--- v3/doble9_engine/lib/doble9_engine/rack.ex
+++ v3/doble9_engine/lib/doble9_engine/rack.ex
@@ -6,7 +6,6 @@ defmodule Doble9Engine.Rack do
     dominoes
     |> sort_numbers_by_count
     |> group_and_arrange_by_number
-#    |> join_groups
     |> flatten
     |> center_doubles
   end
@@ -29,42 +28,6 @@ defmodule Doble9Engine.Rack do
 
   def arrange dominoes, number do
     Enum.map dominoes, fn [h,t] -> number == h && [h,t] || [t,h] end
-  end
-
-  def join_groups [group|groups] do join_groups [group], i groups end
-  def join_groups joined, [] do joined end
-  def join_groups joined, [group|groups] do
-    join_groups join_group(joined, group), groups
-  end
-
-  def join_group [], group do [group] end
-  def join_group [first|rest], group do
-    (number = can_join?(first,group))
-    && [join_group!(first,group)|rest]
-    || [first|join_group(rest,group)]
-  end
-
-  def can_join? joined, group do
-    [h,t] = heads joined
-    can_join?(t, Enum.reverse(joined), group)
-    || can_join?(h, joined, group)
-  end
-  def can_join? number, joined, group do
-    joined
-    |> Enum.take_while(&(number in &1))
-    |> Enum.any?(&(Enum.any? group, fn x -> &1 -- x != [] end))
-  end
-
-  def heads [[h,t]] do [h,t] end
-  def heads group do
-    [repeated(Enum.take group, 2),repeated(group |> Enum.reverse |> Enum.take(2))]
-  end
-
-  def repeated [[h,_],[h,_]] do h end
-  def repeated [[_,t], _] do t end
-
-  def join_group! joined, group do
-
   end
 
   def flatten [] do [] end
